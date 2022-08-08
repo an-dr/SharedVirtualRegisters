8b6a4138e00123c041ee564f7806315c2c47d556
Andrei Gramakov <mail@agramakov.me>
2022-08-08 22:14:45 +0200

    Add a cmake file

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # SharedVirtualRegisters
 
 The library allows to dynamically create a set of thread-save registers
+
+Include the cmake file and use `SRC_SVR` and `INCLUDE_SVR` variables in your CMakeLists.txt
---
+++ SharedVirtualRegisters.cmake
@@ -0,0 +1,16 @@
+# *************************************************************************
+#
+# Copyright (c) 2022 Andrei Gramakov. All rights reserved.
+#
+# This file is licensed under the terms of the MIT license.  
+# For a copy, see: https://opensource.org/licenses/MIT
+#
+# site:    https://agramakov.me
+# e-mail:  mail@agramakov.me
+#
+# *************************************************************************
+
+file (GLOB SRC_SVR "${CMAKE_CURRENT_LIST_DIR}/src/*.c" 
+                   "${CMAKE_CURRENT_LIST_DIR}/src/*.cpp")
+                             
+set (INCLUDE_SVR "${CMAKE_CURRENT_LIST_DIR}/include")
